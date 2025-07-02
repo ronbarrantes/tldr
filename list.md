5833e4471cc0f20ac4df6568a8e5a90c45884a4d
Ron Barrantes <ronbarrantes@gmail.com>
2025-07-02 18:16:39 -0400

    Add initial files

---
+++ list.md
@@ -0,0 +1,10 @@
+## Yes
+
+- https://pru.wd5.myworkdayjobs.com/Careers/job/Newark-NJ-USA/Senior-Software-Engineer_R-121429-1
+- https://pru.wd5.myworkdayjobs.com/en-US/PrudentialTalentAcquisition/job/Software-Engineer_R-120640
+
+## Maybes
+
+- https://pru.wd5.myworkdayjobs.com/en-US/PrudentialTalentAcquisition/job/Senior-Software-Engineer_R-119793
+- https://pru.wd5.myworkdayjobs.com/en-US/PrudentialTalentAcquisition/job/Senior-Software-Engineer-US-Business-Next-Generation-Contact-Center-Team_R-121192
+- https://pru.wd5.myworkdayjobs.com/en-US/PrudentialTalentAcquisition/job/Software-Engineer--EMAS-Customer-Platforms-Team-_R-121167-1
---
+++ popup.js
@@ -0,0 +1,25 @@
+document.getElementById("activate").addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: startClickToSummarize,
+  });
+});
+
+function startClickToSummarize() {
+  document.body.style.cursor = "grab";
+
+  const onClick = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    document.body.style.cursor = "default";
+    const text = e.target.innerText || e.target.textContent || "No text found";
+
+    alert("TLDR:\n\n" + text.slice(0, 300)); // Simulate popup
+    document.removeEventListener("click", onClick, true);
+  };
+
+  document.addEventListener("click", onClick, true);
+}
